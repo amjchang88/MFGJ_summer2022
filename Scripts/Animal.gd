bbfe229b0fd1dad3fa485bb2239d1cbebcf2df46
amjchang88 <37486907+amjchang88@users.noreply.github.com>
2022-07-06 17:17:13 -0500

    fixed shadowed variable issue in Animal.gd

--- Scripts/Animal.gd
+++ Scripts/Animal.gd
@@ -5,13 +5,21 @@ extends Node2D
 
 
 # Basic Animal Stats
+
+# Health stats
 onready var happiness := 0
 onready var health := 0
 onready var alive := true
+onready var affliction := ""
+
+# Static stats
 onready var xpYield := 0
 onready var weight := 0
 onready var height := 0
 
 
-func initialize(species):
-	$AnimatedSprite.animation = species
+func initialize(p_species, p_xpYield, p_affliction):
+	$AnimatedSprite.animation = p_species
+	affliction = p_affliction
+	xpYield = p_xpYield
+	
