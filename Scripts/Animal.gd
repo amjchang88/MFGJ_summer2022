71e75bfd2157bad4b08532050469f6b1573ead36
amjchang88 <amjchang88@gmail.com>
2022-07-13 17:55:31 -0500

    added grayed out versions of animals

--- Scripts/Animal.gd
+++ Scripts/Animal.gd
@@ -29,14 +29,16 @@ func initialize(p_animation, p_xpYield, p_affliction, p_health, p_happiness):
 func _ready():
 	randomize()
 	var randFloat = randf()
-	if randFloat < 0.25:
+	if randFloat < 0.2:
 		$AnimatedSprite.animation = "dog" + str(floor(rand_range(1, 8.99)))
-	elif randFloat < 0.5:
+	elif randFloat < 0.4:
 		$AnimatedSprite.animation = "cat" + str(floor(rand_range(1, 8.99)))
-	elif randFloat < 0.75:
+	elif randFloat < 0.6:
 		$AnimatedSprite.animation = "fish" + str(floor(rand_range(1, 8.99)))
-	elif randFloat <= 1:
+	elif randFloat < 0.8:
 		$AnimatedSprite.animation = "rabbit" + str(floor(rand_range(1, 8.99)))
+	elif randFloat <= 1:
+		$AnimatedSprite.animation = "duck" + str(floor(rand_range(1, 8.99)))
 	$AnimatedSprite.speed_scale = rand_range(0.5, 1.5)
 
 
