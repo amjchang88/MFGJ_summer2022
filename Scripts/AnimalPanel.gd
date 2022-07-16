fcf2007ea8073827e064a6dab0fd84dfa1db18bb
amjchang88 <amjchang88@gmail.com>
2022-07-16 12:46:59 -0500

    cleaner set_entries function, added frog shadow anim

--- Scripts/AnimalPanel.gd
+++ Scripts/AnimalPanel.gd
@@ -4,6 +4,7 @@ signal returned
 
 var nameList = [" ", "", " ", " ", " ", " ", " ", " "]
 var children 
+var variationList
 
 func _ready():
 	children = $AnimalList.get_children()
@@ -16,6 +17,7 @@ func _on_Area2D_input_event(_viewport, event, _shape_idx):
 			emit_signal("returned")
 
 func initialize(p_name, p_variationList, p_nameList, p_rarity):
+	variationList = p_variationList
 	$JournalTitle.bbcode_text = "[center]" + p_name + "[/center]"
 	$StarSheet.frame = p_rarity
 	
@@ -27,35 +29,26 @@ func initialize(p_name, p_variationList, p_nameList, p_rarity):
 		else:
 			children[i].animation = p_name.to_lower() + "Shadow"
 	
+func set_entries(i):
+	if variationList[i-1]:
+		$NameText.text = nameList[0]
+	else:
+		$NameText.text = "Undiscovered"
 
-
+# calling set_entries function
 func _on_1_clicked():
-	$NameText.text = nameList[0]
-
-
+	set_entries(1)
 func _on_2_clicked():
-	$NameText.text = nameList[1]
-
-
+	set_entries(2)
 func _on_3_clicked():
-	$NameText.text = nameList[2]
-
-
+	set_entries(3)
 func _on_4_clicked():
-	$NameText.text = nameList[3]
-
-
+	set_entries(4)
 func _on_5_clicked():
-	$NameText.text = nameList[4]
-
-
+	set_entries(5)
 func _on_6_clicked():
-	$NameText.text = nameList[5]
-
-
+	set_entries(6)
 func _on_7_clicked():
-	$NameText.text = nameList[6]
-
-
+	set_entries(7)
 func _on_8_clicked():
-	$NameText.text = nameList[7]
+	set_entries(8)
