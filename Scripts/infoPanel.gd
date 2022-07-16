c96b1915e85e5d5db1ce9310bf72a140e0d74a98
amjchang88 <amjchang88@gmail.com>
2022-07-16 15:23:28 -0500

    update animal functions and main ui

--- Scripts/infoPanel.gd
+++ Scripts/infoPanel.gd
@@ -1,5 +1,10 @@
 extends UIPanel
 	
-func _ready():
-	#move_to(2,2)
-	$AnimatedSprite.animation = "dog3"
+func set_values(p_animalName, p_nickname, p_weight, p_symptom, p_affliction, p_gender, p_rarity):
+	$Name.bbcode_text = "[center]" + p_nickname + "[/center]"
+	$SpeciesText.text = p_animalName
+	$WeightText.text = str(p_weight)
+	$StatusText.text = p_symptom
+	$AfflictionText.text = p_affliction
+	$GenderIcon.frame = p_gender
+	$StarSheet.frame = p_rarity
