91bb100d9893b9d1b01bf6fc2cc4860721e34765
amjchang88 <amjchang88@gmail.com>
2022-07-16 12:34:32 -0500

    set_panel function, journal page for all animals

--- Scripts/JournalPanel.gd
+++ Scripts/JournalPanel.gd
@@ -21,11 +21,35 @@ func _on_Area2D_input_event(_viewport, event, _shape_idx):
 func _on_AnimalPanel_returned():
 	paused = false
 	
-func _on_Cat_clicked():
+func set_panel(p_name : String):
+	var localName : String = global.get(p_name + "Class").name
+	var localVariationList = global.get(p_name + "Class").variationList
+	var localNameList = global.get(p_name + "Class").nameList
+	var localRarity = global.get(p_name + "Class").rarity
+	
 	if !paused:
 		animalPanel.move_to(112, 2)
-		animalPanel.initialize(global.catClass.name, global.catClass.variationList, global.catClass.nameList, global.catClass.rarity)
+		animalPanel.initialize(localName, localVariationList, localNameList, localRarity)
 		paused = true
-
-
-
+		
+# calling set panel functions
+func _on_Cat_clicked():
+	set_panel("cat")
+func _on_Dog_clicked():
+	set_panel("dog")
+func _on_Fish_clicked():
+	set_panel("fish")
+func _on_Rabbit_clicked():
+	set_panel("rabbit")
+func _on_Duck_clicked():
+	set_panel("duck")
+func _on_Rat_clicked():
+	set_panel("rat")
+func _on_Frog_clicked():
+	set_panel("frog")
+func _on_Bird_clicked():
+	set_panel("bird")
+func _on_Axolotl_clicked():
+	set_panel("axolotl")
+func _on_Dragon_clicked():
+	set_panel("dragon")
