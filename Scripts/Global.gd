497ae23df9ee713239c666cce1ff595cd18bd306
maryannejpark <80556291+maryannejpark@users.noreply.github.com>
2022-07-16 23:55:52 -0500

    symptoms

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -208,63 +208,69 @@ func _init():
 	catClass.weightLow = 7
 	catClass.weightHigh = 18
 	catClass.nameList = ["Black Shorthair", "Tuxedo Shorthair", "Coffee Longhair", "Calico Shorthair", "Brown Tabby", "Orange Shorthair", "Chocolate Point", "White Longhair"]
-	catClass.symptomList = ["Fever", "Rashes", "Itching", "Vomiting", "Limping"]
+	catClass.symptomList = ["Fever", "Rashes", "Itching", "Vomiting", "Limping", "Bleeding", "Swelling", "Lethargy"]
 	
 	dogClass.name = "Dog"
 	dogClass.rarity = 0
 	dogClass.weightLow = 40
 	dogClass.weightHigh = 85
 	dogClass.nameList = ["Border Collie", "Chocolate Lab", "Rhodesian Ridgeback", "Golden Retriever", "Terrier", "Black Shiba Inu", "Schipperke", "White German Shepherd"]
-	dogClass.symptomList = ["Fever", "Rashes", "Itching", "Vomiting", "Limping"]
+	dogClass.symptomList = ["Fever", "Rashes", "Itching", "Vomiting", "Limping", "Bleeding", "Swelling", "Lethargy"]
 	
 	fishClass.name = "Fish"
 	fishClass.rarity = 1
 	fishClass.weightLow = 0.1
 	fishClass.weightHigh = 1
 	fishClass.nameList = ["Blue Beta", "Light Blue Beta", "Orange Guppy", "Red Beta", "Yellow Guppy", "Orange Koi", "Red Koi", "Tricolor Koi"]
+	fishClass.symptomList = ["Frayed fins", "Weight loss", "Gasping"]
 	
 	duckClass.name = "Duck"
 	duckClass.rarity = 1
 	duckClass.weightLow = 1
 	duckClass.weightHigh = 4
 	duckClass.nameList = ["Black", "Brown", "Mallard (Female)", "Mallard (Male)", "Mandarin (Female)", "Mandarin (Male)", "White", "Yellow"]
+	duckClass.symptomList = ["Eye crusts", "Damaged feathers", "Sneezing", "Beak lesions"]
 	
 	rabbitClass.name = "Rabbit"
 	rabbitClass.rarity = 1
 	rabbitClass.weightLow = 2
 	rabbitClass.weightHigh = 10
 	rabbitClass.nameList = ["Black", "Dutch", "Brown", "Californian", "Harlequin", "White", "Golden"]
-	rabbitClass.symptomList = ["Fever", "Rashes", "Itching", "Vomiting", "Limping"]
+	rabbitClass.symptomList = ["Fever", "Rashes", "Itching", "Vomiting", "Limping", "Bleeding", "Swelling", "Lethargy"]
 	
 	ratClass.name = "Rat"
 	ratClass.rarity = 2
 	ratClass.weightLow = 0.5
 	ratClass.weightHigh = 1.5
 	ratClass.nameList = ["Black", "Black Piebald", "Blue", "Gray", "Gray Piebald", "Brown", "Brown Piebald", "Tan Piebald"]
-	ratClass.symptomList = ["Fever", "Rashes", "Itching", "Vomiting", "Limping"]
+	ratClass.symptomList = ["Fever", "Rashes", "Itching", "Vomiting", "Limping", "Bleeding", "Swelling", "Lethargy"]
 	
 	frogClass.name = "Frog"
 	frogClass.rarity = 2
 	frogClass.weightLow = 0.1
 	frogClass.weightHigh = 5
 	frogClass.nameList = ["Brown", "Green", "Tree", "White", "Golden", "Blue Poison", "Red Poison", "Yellow Poison"]
+	frogClass.symptomList = ["Dry skin", "Dull skin", "Cloudy eyes"]
 	
 	birdClass.name = "Bird"
 	birdClass.rarity = 3
 	birdClass.weightLow = 0.1
 	birdClass.weightHigh = 1
 	birdClass.nameList = ["Red Canary", "Yellow Canary", "Gray Cockatiel", "White Cockatiel", "Green Lovebird", "Rainbow Lovebird", "Blue Parakeet", "Green Parakeet"]
+	birdClass.symptomList = ["Eye crusts", "Damaged feathers", "Sneezing", "Beak lesions"]
 	
 	axolotlClass.name = "Axolotl"
 	axolotlClass.rarity = 3
 	axolotlClass.weightLow = 0.1
 	axolotlClass.weightHigh = 1
 	axolotlClass.nameList = ["Black", "Copper", "Golden", "Lavender", "Piebald", "Pink", "Sunglasses", "Red"]
+	axolotlClass.symptomList = ["Bloating", "Fuzzy gills", "Floating"]
 	
 	dragonClass.name = "Dragon"
 	dragonClass.rarity = 4
 	dragonClass.weightLow = 5
 	dragonClass.weightHigh = 15
 	dragonClass.nameList = ["Air", "Earth", "Fire", "Frost", "Light", "Shadow", "Soul", "Water"]
+	dragonClass.symptomList = ["Discolored scales", "Coughing", "Irritability", "Dry claws"]
 
 	symptomDict = {"Fever" : fever, "Rashes" : rashes, "Itching" : itching, "Vomiting" : vomiting, "Limping" : limping, "Bleeding" : bleeding, "Swelling" : swelling, "Lethargy" : lethargy, "Eye crusts" : eyeCrusts, "Damaged feathers" : damagedFeathers, "Sneezing" : sneezing, "Beak lesions" : beakLesions, "Frayed fins" : frayedFins, "Weight loss" : weightLoss, "Gasping" : gasping, "Dry skin" : drySkin, "Dull skin" : dullSkin, "Cloudy eyes" : cloudyEyes, "Bloating" : bloating, "Fuzzy gills" : fuzzyGills, "Floating" : floating, "Discolored scales" : discoloredScales, "Coughing" : coughing, "Irritability" : irritability, "Dry claws" : dryClaws}
